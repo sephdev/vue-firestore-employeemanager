79c5c4a9eda3b6b5159871b64d8dbd3752f19737
Joseph Tandingan <josephdevapps@gmail.com>
2018-09-04 01:01:56 +0800

    View Employee, Delete

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDYmuQ6d_Fd0O2rdlED5PUsqQ5QRUGpUf0",
+    authDomain: "vue-fs-prod-4a97c.firebaseapp.com",
+    databaseURL: "https://vue-fs-prod-4a97c.firebaseio.com",
+    projectId: "vue-fs-prod-4a97c",
+    storageBucket: "vue-fs-prod-4a97c.appspot.com",
+    messagingSenderId: "942849467509"
+}
