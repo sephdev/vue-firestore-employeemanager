0a67990dc3751d6bc89d29b5f77e1b4b7b6dfba7
Joseph Tandingan <josephdevapps@gmail.com>
2018-09-04 01:21:54 +0800

    Update Employee/Entry

--- src/components/ViewEmployee.vue
+++ src/components/ViewEmployee.vue
@@ -2,12 +2,20 @@
     <div id="view-employee">
         <ul class="collection with-header">
             <li class="collection-header"><h4>{{name}}</h4></li>
-            <li class="collection-item">Employee ID#:{{employee_id}}</li>
-            <li class="collection-item">Department:{{dept}}</li>
-            <li class="collection-item">Position:{{position}}</li>
+            <li class="collection-item">Employee ID#: {{employee_id}}</li>
+            <li class="collection-item">Department: {{dept}}</li>
+            <li class="collection-item">Position: {{position}}</li>
         </ul>
         <router-link to="/" class="btn grey">Back</router-link>
         <button @click="deleteEmployee" class="btn red">Delete</button>
+
+        <div class="fixed-action-btn">
+            <router-link v-bind:to="{name: 'edit-employee', params: {employee_id: employee_id}}" class="btn-floating btn-large red">
+                <i class="fa fa-pencil"></i>
+
+            </router-link>
+        </div>
+
     </div>
 </template>
 
